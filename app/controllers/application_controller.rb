041ca80a29563af4301fe40eceb69968ef22fd83
Hiroaki Machitani <d.dhm0824@gmail.com>
2020-05-23 03:37:59 +0900

    #9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, only: [:show, :create, :update, :destroy]
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_search
 
   def after_sign_in_path_for(resource)
     user_path(current_user.id)
+  end
+
+  def set_search
+    @search = Post.ransack(params[:q])
+    @search_posts = @search.result(distinct: true).page(params[:page]).per(20)
   end
 
   protected

1c8f44618f87f4d7d595c1cc1d7325c0e1ba3ead
Hiroaki Machitani <d.dhm0824@gmail.com>
2020-07-30 12:51:00 +0900

    いいねランキング機能の実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @recent_posts = Post.all.order(created_at: :desc).limit(3)
+    @higher_rank = Post.create_higher_rank
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -86,4 +86,8 @@ class Post < ApplicationRecord
       notification.save if notification.valid?
     end
   end
+
+  def self.create_higher_rank
+    Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(10).pluck(:post_id))
+  end
 end
